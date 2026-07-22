73ae55e23dddc252b3f17bf666814a5aeb643349
devachandar <47187469+devachandar@users.noreply.github.com>
2026-07-22 16:18:42 -0400

    Delete media-service directory

--- media-service/mediaapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("upload", views.UploadView.as_view()),
-    path("uploads/<str:filename>", views.ServeUploadView.as_view()),
-]
